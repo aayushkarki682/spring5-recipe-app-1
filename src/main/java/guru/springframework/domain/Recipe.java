407c4b5c27b1528961e2a867cad952ad7d7b11ea
aayushkarki682 <ayush.karki12345@gmail.com>
2021-02-20 18:52:44 -0600

    Tests, converters and command classes added.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -128,7 +128,7 @@ public class Recipe {
     public void setNotes(Notes notes) {
 
         this.notes = notes;
-        notes.setRecipe(this);
+
     }
 
     public Recipe addIngredients(Ingredient ingredient){
